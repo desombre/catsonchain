064c206fa0dba484a70d53926cbeb0332f7d035e
Philipp de Sombre <philipp@desomb.re>
2022-02-01 20:19:25 +0100

    Add random cat profle gif

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./Profile.css";
 
 const Profile = () => {
+  const [catId, setCatId] = useState(Math.floor(Math.random() * 10));
+
   return (
     <div className="profileContainer">
-      <div className="profile">
-        <img src="profile.gif" alt="cat gif"></img>
+      <div
+        className="profile"
+        onDoubleClick={() =>
+          setCatId((oldID) => {
+            let newID = Math.floor(Math.random() * 10);
+            while (oldID === newID) newID = Math.floor(Math.random() * 10);
+            return newID;
+          })
+        }
+      >
+        <img src={`cat${catId}.gif`} alt="cat gif"></img>
       </div>
     </div>
   );
